2ec490f5ba447a29b4c235ec7d06f478cb02ec9e
Matheus de Carvalho <matheusc.j@hotmail.com>
2023-06-20 23:19:21 -0300

    feat: created the Listagem page

---
+++ src/app/Listagem/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/Button/'
+import { List } from '@/components/List/List'
+import Link from 'next/link'
+
+export default function Listagem() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center align-middle">
+      <List renderList={['ola', 'teste']} />
+      <Link href="/">
+        <Button text="Ir para Home" />
+      </Link>
+    </div>
+  )
+}
