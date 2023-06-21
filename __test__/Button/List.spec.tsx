84c6507917c61e429d89429ff724d87559262aae
Matheus de Carvalho <matheusc.j@hotmail.com>
2023-06-20 23:20:55 -0300

    feat: create List component test

---
+++ __test__/Button/List.spec.tsx
@@ -0,0 +1,18 @@
+import { List } from '@/components/List/List'
+import { render, screen } from '@testing-library/react'
+
+describe('Should render a List with all props passed', () => {
+  test('Should render all elements in renderList props', () => {
+    const listToRender = ['primeiro', 'segundo']
+    render(<List renderList={listToRender} />)
+
+    const listaElement = screen.getByRole('list')
+    expect(listaElement).toBeInTheDocument()
+
+    const itemElements = screen.getAllByRole('listitem')
+    expect(itemElements.length).toBe(listToRender.length)
+
+    const itemTexts = itemElements.map((item) => item.textContent)
+    expect(itemTexts).toEqual(listToRender)
+  })
+})
